1d2f554a5c8dc2b9e85be34e0ef3a2f5ced97daf
Lavina <150042947+lavinad04@users.noreply.github.com>
2024-03-02 20:09:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-//your JS code here. If required.
+let otpCon = document.getElementById("otpContainer");
+
+otpCon.addEventListener("click", otpContainer)
+
+function otpContainer(){
+	let container = document.createElement("div");
+	container.className = "code-container";
+	otpContainer.append("div");
+	
+
+
+
+
+
+	
+}
